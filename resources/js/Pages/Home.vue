83f6b1f476b925b62e59e2d504509657ac14f0da
Nikolay0525 <annaksota361@gmail.com>
2026-09-15 16:13:11 +0300

    Created fresh laravel project and added all frameworks vue, vite, tailwind

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="min-h-screen bg-gray-100 px-6 py-12">
+        <main class="mx-auto max-w-4xl rounded-xl bg-white p-8 shadow-lg">
+            <h1 class="text-3xl font-bold text-gray-900">
+                Hello world!
+            </h1>
+        </main>
+    </div>
+</template>
