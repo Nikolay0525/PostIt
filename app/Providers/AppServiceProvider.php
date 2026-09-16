c8d60af4614346379ca2b52055e673dd6f400235
Nikolay0525 <annaksota361@gmail.com>
2026-09-16 15:21:06 +0300

    Added neccessary tables before creating user settings, fixed missing "use" in app\providers.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use App\Observers\UserObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
--- database/migrations/2026_09_16_112202_create_user_settings_table.php
+++ database/migrations/2026_09_16_112202_create_user_settings_table.php
@@ -8,6 +8,18 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Schema::create('ui_languages', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('code', 10);
+            $table->string('name', 50);
+            $table->boolean('is_active')->default(true);
+        });
+
+        Schema::create('speaking_languages', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 50);
+        });
+
         Schema::create('user_settings', function (Blueprint $table) {
             $table->uuid('user_id')->primary();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
@@ -16,7 +28,7 @@ return new class extends Migration
             $table->foreignUuid('speaking_language_id')->constrained('speaking_languages');
 
             $table->boolean('dark_theme')->default(false);
-            $table->boolean('show_swear_words')->default(true);
+            $table->boolean('show_swear_words')->default(false);
             $table->boolean('show_adult_content')->default(false);
             $table->boolean('enable_cookies')->default(false);
             $table->boolean('allow_messages')->default(true);
@@ -28,5 +40,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('user_settings');
+        Schema::dropIfExists('speaking_languages');
+        Schema::dropIfExists('ui_languages');
     }
 };
