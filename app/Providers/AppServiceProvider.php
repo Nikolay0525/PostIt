61017b49ff49a818d8101e69e77b48d1570c26af
Nikolay0525 <annaksota361@gmail.com>
2026-09-22 00:13:39 +0300

    Added comment repository and service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
             \App\Repositories\Contracts\PostRepositoryInterface::class,
             \App\Repositories\Eloquent\EloquentPostRepository::class
         );
+
+        $this->app->bind(
+            \App\Repositories\Contracts\CommentRepositoryInterface::class,
+            \App\Repositories\Eloquent\EloquentCommentRepository::class
+        );
     }
 
     /**
---
+++ app/Services/CommentService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\CommentRepositoryInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class CommentService
+{
+    private const PER_PAGE = 20;
+
+    public function __construct(
+        protected CommentRepositoryInterface $commentRepository
+    ) {}
+
+    public function getPostThreads(string $postId): LengthAwarePaginator
+    {
+        return $this->commentRepository->paginateThreadsForPost($postId, self::PER_PAGE);
+    }
+}
